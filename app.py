7de4cc3610c97ce1542f4d7480f638ad123f6c3a
Liam Ketchup <lketchup@thoughtworks.com>
2016-12-09 20:45:51 +1100

    Heroku/Postgres setup

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run()

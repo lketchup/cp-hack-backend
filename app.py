eaace649e9bfbdfedc4a885e100e466d973e93c0
atrotors <dev@erfan.io>
2016-12-10 20:38:20 +1100

    tweak product api

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
-@app.route('/groups/<int:index>/<int:size>/', methods=['GET', 'POST'])
-def groups(index, size):
+@app.route('/groups/<string:token>/<int:index>/<int:size>/', methods=['GET', 'POST'])
+def groups(token, index, size):
     if request.method == 'GET':
         return jsonify([
             {
@@ -45,8 +45,8 @@ def groups(index, size):
     elif request.method == 'POST':
         return jsonify(request.get_json())
 
-@app.route('/groups/<int:id>/<int:index>/<int:size>/', methods=['GET', 'PUT'])
-def group(id, index, size):
+@app.route('/groups/<string:token>/<int:id>/<int:index>/<int:size>/', methods=['GET', 'PUT'])
+def group(token, id, index, size):
     if request.method == 'GET':
         return jsonify({
             'id': id,
@@ -69,68 +69,94 @@ def group(id, index, size):
     elif request.method == 'PUT':
         return jsonify(dict({'id': id}, **request.get_json()))
 
-@app.route('/profiles/', methods=['POST'])
-def profiles():
+@app.route('/profiles/<string:token>/', methods=['POST'])
+def profiles(token):
     return jsonify(request.get_json())
 
-@app.route('/profiles/<int:id>/<int:index>/<int:size>/', methods=['GET', 'POST'])
-def profile(id, index, size):
+@app.route('/profiles/<string:token>/<int:id>/', methods=['GET', 'POST'])
+def profile(token, id, index, size):
     if request.method == 'GET':
         return jsonify({
             'id': id,
             'name': 'Ramzi',
             'avatar_url': 'https://avatars1.githubusercontent.com/u/9994172?v=3&s=40',
             'hero_url': 'http://flask.pocoo.org/docs/0.11/_images/debugger.png',
+            'email': 'ramzi',
             'interests': [
                 'arab fighting',
                 'not coding',
                 'entering data into unresponsive excel',
             ],
-            'products': [
-                {
-                    'id': 1,
-                    'name': 'Ramzi action figure',
-                    'url': 'http://google.com/',
-                    'img_url': 'http://flask.pocoo.org/docs/0.11/_static/flask.png',
-                    'price': 3.99,
-                    'description': '3D printed ramzi action figure!',
-                },
-                {
-                    'id': 2,
-                    'name': 'Ramzi action figure',
-                    'url': 'http://google.com/',
-                    'img_url': 'http://flask.pocoo.org/docs/0.11/_static/flask.png',
-                    'price': 3.99,
-                    'description': '3D printed ramzi action figure!',
-                },
-                {
-                    'id': 3,
-                    'name': 'Ramzi action figure',
-                    'url': 'http://google.com/',
-                    'img_url': 'http://flask.pocoo.org/docs/0.11/_static/flask.png',
-                    'price': 3.99,
-                    'description': '3D printed ramzi action figure!',
-                },
-                {
-                    'id': 4,
-                    'name': 'Ramzi action figure',
-                    'url': 'http://google.com/',
-                    'img_url': 'http://flask.pocoo.org/docs/0.11/_static/flask.png',
-                    'price': 3.99,
-                    'description': '3D printed ramzi action figure!',
-                },
-                {
-                    'id': 5,
-                    'name': 'Ramzi action figure',
-                    'url': 'http://google.com/',
-                    'img_url': 'http://flask.pocoo.org/docs/0.11/_static/flask.png',
-                    'price': 3.99,
-                    'description': '3D printed ramzi action figure!',
-                }
-            ]
         })
     elif request.method == 'POST':
         return jsonify(dict({'id': id}, **request.get_json()))
+
+@app.route('/products/<string:token>/<int:profile_id>/<int:index>/<int:count>/', methods=['GET'])
+def products(token, profile_id, index, count):
+    return jsonify(
+        [
+            {
+                'id': 1,
+                'name': 'Ramzi action figure',
+                'url': 'http://google.com/',
+                'img_url': 'http://flask.pocoo.org/docs/0.11/_static/flask.png',
+                'price': 3.99,
+                'description': '3D printed ramzi action figure!',
+                'owner': {
+                    'name': 'Target',
+                    'logo_url': 'https://astridrodriguez.files.wordpress.com/2013/05/target-logo.png',
+                }
+            },
+            {
+                'id': 2,
+                'name': 'Ramzi action figure',
+                'url': 'http://google.com/',
+                'img_url': 'http://flask.pocoo.org/docs/0.11/_static/flask.png',
+                'price': 3.99,
+                'description': '3D printed ramzi action figure!',
+                'owner': {
+                    'name': 'Target',
+                    'logo_url': 'https://astridrodriguez.files.wordpress.com/2013/05/target-logo.png',
+                }
+            },
+            {
+                'id': 3,
+                'name': 'Ramzi action figure',
+                'url': 'http://google.com/',
+                'img_url': 'http://flask.pocoo.org/docs/0.11/_static/flask.png',
+                'price': 3.99,
+                'description': '3D printed ramzi action figure!',
+                'owner': {
+                    'name': 'Target',
+                    'logo_url': 'https://astridrodriguez.files.wordpress.com/2013/05/target-logo.png',
+                }
+            },
+            {
+                'id': 4,
+                'name': 'Ramzi action figure',
+                'url': 'http://google.com/',
+                'img_url': 'http://flask.pocoo.org/docs/0.11/_static/flask.png',
+                'price': 3.99,
+                'description': '3D printed ramzi action figure!',
+                'owner': {
+                    'name': 'Target',
+                    'logo_url': 'https://astridrodriguez.files.wordpress.com/2013/05/target-logo.png',
+                }
+            },
+            {
+                'id': 5,
+                'name': 'Ramzi action figure',
+                'url': 'http://google.com/',
+                'img_url': 'http://flask.pocoo.org/docs/0.11/_static/flask.png',
+                'price': 3.99,
+                'description': '3D printed ramzi action figure!',
+                'owner': {
+                    'name': 'Target',
+                    'logo_url': 'https://astridrodriguez.files.wordpress.com/2013/05/target-logo.png',
+                }
+            }
+        ]
+    )
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
